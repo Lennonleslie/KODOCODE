8177fc0e3d19214f7e462244d00d518bc319a3f7
lennon.leslie <lennon.leslie@hotmail.com>
2024-06-02 21:51:09 +0800

    修改主页，增加git教程页面。

--- upload.sh
+++ upload.sh
@@ -1,4 +1,13 @@
+# 使用此脚本。需要配置好所有的配置文件
+# 然后再git页面内 ./upload.sh
+
+
+
+
 # 自动更新部署
 git add .
-git commit -m '调整主页'
-git push
+# 记得更改提交信息
+git commit -m '修改主页，增加git教程页面。'
+# git push
+# 上传到远程仓库中的main分支
+git push origin main
